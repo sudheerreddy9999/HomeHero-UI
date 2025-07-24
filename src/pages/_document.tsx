8d2dbf15e7c29b604ae8a6cafa5c3c8c8c177443
Sudheer Reddy <sudheerjanga9999@gmail.com>
2025-07-24 11:52:42 +0530

    metaTag Added

---
+++ src/pages/_document.tsx
@@ -0,0 +1,19 @@
+import Document, { Html, Head, Main, NextScript } from 'next/document';
+
+class MyDocument extends Document {
+  render() {
+    return (
+      <Html lang="en">
+        <Head>
+          <meta name="fast2sms" content="i17pYICdCf96udk85RG6ty0idh8J2DQR" />
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
+}
+
+export default MyDocument;
